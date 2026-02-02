8c8efda4d81b2b5f8c4252d81a7d1569e00b01fa
Jesus Hernandez <jesushdzusa@gmail.com>
2026-02-02 17:13:37 -0600

    env variable

--- src/services/requests.jsx
+++ src/services/requests.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
-
-const baseUrl = 'http://localhost:10000'
+const api = import.meta.env.VITE_API_URL;
+const baseUrl = api;
+console.log("API URL:", baseUrl);
 
 const getClients = async () => {
     const request = await axios.get(`${baseUrl}/clientes`)
